fc3eac89c1166caf3e012ebca7f9bcdbeb9d1f8b
t-numata <2x.pypy.ktr@gmail.com>
2018-10-05 13:04:27 +0900

    関数への分割代入

---
+++ p111.js
@@ -0,0 +1,15 @@
+function getSentence({subject, verb, object}) {
+    return `${subject} ${verb} ${object}`;
+}
+
+const o = {
+    subject: "I",
+    verb: "love",
+    object: "you"
+};
+
+console.log(getSentence(o));
+/**
+ * 分割代入を使えば、一気に変数への代入が可能
+ * 配列でも同じことができる
+ */
