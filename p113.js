7c6b300950f8b75dae65d4853636cdcc2b6daf8b
t-numata <2x.pypy.ktr@gmail.com>
2018-10-05 13:19:27 +0900

    オブジェクト内での関数宣言とショートハンドについて

---
+++ p113.js
@@ -0,0 +1,24 @@
+const obj = {
+    name: "Tony Stark",
+    spoil: function() {
+        return "I am iron man";
+    }
+}
+
+console.log(obj);
+console.log(obj.spoil());
+
+const shorthandObj = {
+    name: "Tony Stark",
+    spoil() {
+        return "I am iron man";
+    }
+}
+
+console.log(shorthandObj);
+console.log(shorthandObj.spoil());
+
+/**
+ * オブジェクト内のプロパティとして関数を瀬戸することができる
+ * ショートハンドを使うとより短縮して書くことができる
+ */
