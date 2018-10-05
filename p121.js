354c2643b1885d0b77aa27827fb1fd98244e5254
t-numata <2x.pypy.ktr@gmail.com>
2018-10-05 14:52:35 +0900

    bindの使い方call applyとの違いについて

---
+++ p121.js
@@ -0,0 +1,32 @@
+const quill = {name: "Quill"};
+const steve = {name: "Steve"};
+
+function update(team, heroname) {
+    this.team = team;
+    this.heroname = heroname;
+}
+
+const updateQuill = update.bind(quill);
+
+console.log(quill);
+updateQuill("GotG", "StarLoad");
+console.log(quill);
+
+
+console.log(quill);
+updateQuill("GotG", "StarLoad");
+console.log(quill);
+
+
+console.log(steve);
+updateQuill.call(steve, "Avengers", "Captain America");
+console.log(steve);
+console.log(quill);
+
+/**
+ * bindではthisの内容を永続的に固定する
+ * 上記の場合、updateQuillのthisはquillオブジェクトに固定されているため、
+ * updateQuill.call(steve, "Avengers", "Captain America");
+ * にてsteveを指定することができず
+ * quilがAvengers Captain Americaとなる
+ */
