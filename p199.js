1487dddc661de0c2f397e32307227a7abbeb50d4
t-numata <2x.pypy.ktr@gmail.com>
2018-10-09 11:52:36 +0900

    イテレータについて

---
+++ p199.js
@@ -0,0 +1,27 @@
+const members = [
+    "Stark",
+    "Steve",
+    "Peter",
+    "Quil",
+    "Hulk",
+];
+
+const iteraterTest1 = members.values();
+console.log(iteraterTest1.next());
+console.log(iteraterTest1.next());
+console.log(iteraterTest1.next());
+console.log(iteraterTest1.next());
+console.log(iteraterTest1.next());
+console.log(iteraterTest1.next());
+
+
+const iteraterTest2 = members.values();
+let current = iteraterTest2.next();
+while (!current.done) {
+    console.log(current.value);
+    current = iteraterTest2.next();
+}
+
+/**
+ * イテレータの使い方
+ */
