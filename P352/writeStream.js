b73ced2a6dd2f26adf8d0d7acc13fdfa5b0803ca
t-numata <2x.pypy.ktr@gmail.com>
2018-10-10 15:46:21 +0900

    書き込みストリームについて

---
+++ P352/writeStream.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const writeStream = fs.createWriteStream(__dirname + '/stream.txt', {encoding: 'utf-8'});
+writeStream.write('1st line \n');
+writeStream.write('2nd line \n');
+writeStream.write('3rd line \n');
+
+writeStream.end();
+
+/**
+ * ファイルへの書き込みのストリーム
+ */
