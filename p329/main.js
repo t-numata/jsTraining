1c4ecf7dcd2963f8ef6e22402a3ff20c896dd205
t-numata <2x.pypy.ktr@gmail.com>
2018-10-10 10:55:58 +0900

    jQueryの基本

---
+++ p329/main.js
@@ -0,0 +1,8 @@
+$(function() {
+    const $param = $('p');
+    console.log($param.length);
+    console.log(typeof $param);
+    console.log($param.length);
+    console.log($param instanceof $);
+    console.log($param instanceof jQuery);
+})
