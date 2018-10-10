f359cb7b28221b46a782de0a63e37875aa7b556e
t-numata <2x.pypy.ktr@gmail.com>
2018-10-10 10:58:43 +0900

    特定の要素すべてを更新するとき

--- p329/main.js
+++ p329/main.js
@@ -5,4 +5,7 @@ $(function() {
     console.log($param.length);
     console.log($param instanceof $);
     console.log($param instanceof jQuery);
+
+    // すべてのp要素の文字列を変更する
+    $('p').text("更新されました！"");
 })
