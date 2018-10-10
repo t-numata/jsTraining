d510873e23cd036fe99e1de931dd526e3873657e
t-numata <2x.pypy.ktr@gmail.com>
2018-10-10 13:04:36 +0900

    jQueryを使ったajaxについて

---
+++ p333/main.js
@@ -0,0 +1,23 @@
+$(function () {
+    const $weatherTag = $(".weather > p");
+    const $locationTag = $(".location > p");
+    $weatherTag.text("情報の取得中...");
+    $locationTag.text("情報の取得中...");
+    let TimeId = setInterval(reloadServerInfo($weatherTag, $locationTag), 500 * 1000);
+    function reloadServerInfo($weatherTag, $locationTag) {
+      $.getJSON(
+        "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040",
+        function(data) {
+            weatherData = data.forecasts;
+            $weatherTag.text(weatherData[0].telop);
+            $locationTag.text(data.location.city);
+        }
+      )
+    }
+});
+/**
+ * ajax通信について
+ * このjsローカルで動かす際には、Access-Control-Allow-Originを回避してください
+ * https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi/related?utm_source=chrome-app-launcher-info-dialog
+ * 
+ */
