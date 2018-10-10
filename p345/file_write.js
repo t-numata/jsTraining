1a28870046f20a9a49025f166f273b915942aed9
t-numata <2x.pypy.ktr@gmail.com>
2018-10-10 14:43:06 +0900

    非同期処理を用いたファイルの読み書きについて

---
+++ p345/file_write.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+const message = 'ファイルへ書き込みをしましたよ\n';
+try {
+    fs.writeFileSync(path.join(__dirname, 'hello.txt'),message);
+} catch(err) {
+    console.error('ファイルに書き込みができませんでした');
+}
