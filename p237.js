eed3632119106c247164ecbc4336a227c7b1f497
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 11:14:24 +0900

    関数をすべてアロー関数化

--- p237.js
+++ p237.js
@@ -1,21 +1,21 @@
 function countdown(seconds) {
-    return new Promise(
-        function (onFulfilled, onRejected) {
-            for(let i = seconds; i >= 0; i--) {
-                setTimeout(function() {
-                    if (i === 13) return onRejected(new Error('数字が呪われている'));
-                    if(i > 0) console.log(i + '...');
-                    else onFulfilled(console.log('GO'));
-                }, (seconds - 1 ) * 1000);
-            }
-        });
+    return new Promise((onFulfilled, onRejected) => {
+        for (let i = seconds; i >= 0; i--) {
+            setTimeout(() => {
+                if (i === 13)
+                    return onRejected(new Error('数字が呪われている'));
+                if (i > 0) console.log(`${i}...`);
+                else onFulfilled(console.log('GO'));
+            }, (seconds - 1) * 1000);
+        }
+    });
 }
 countdown(15).then(
-    function() {
+    () => {
         console.log('カウントダウン成功！');
     },
-    function (err) {
-        console.log('カウントダウンエラー発生' + err.message);
+    err => {
+        console.log(`カウントダウンエラー発生${err.message}`);
     }
 );
 
