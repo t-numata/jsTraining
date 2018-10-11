1e4a518ba443a8add6d061bef85dde7db7e66040
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 13:05:50 +0900

    symbolへのdescription追加

--- p39.js
+++ p39.js
@@ -1,6 +1,7 @@
-const RED = Symbol();
-const BLUE = Symbol();
-const ORANGE = Symbol('夕日の色');
+// symbolにはdescriptionを追加すること
+const RED = Symbol('test for red');
+const BLUE = Symbol('test for blue');
+const ORANGE = Symbol('test for orange');
 
 console.log(RED);
 console.log(BLUE);
