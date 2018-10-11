6e8e7c2712d8f74b210356b831677505665219ed
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 17:46:04 +0900

    イテレータを使用しないように変更

--- p201.js
+++ p201.js
@@ -3,39 +3,32 @@ class Log {
         this.messages = [];
     }
 
-    add(message) {
+    addMessage(message) {
         const now = Date.now();
         console.log(`ログ追加: ${message} (${now})`);
         this.messages.push({ message, timestamp: now });
     }
 
-    [Symbol.iterator]() {
-        return this.messages.values();
+    getMessages() {
+        return this.messages;
     }
 }
 
 const log = new Log();
-log.add('海の監視開始');
+log.addMessage('海の監視開始');
 
-setTimeout(() => {
-    log.add('クジラを見た'), 3 * 1000;
-});
-setTimeout(() => {
-    log.add('船を見た'), 4 * 1000;
-});
-setTimeout(() => {
-    log.add('メガロドンが来た'), 5 * 1000;
-});
-setTimeout(() => {
-    log.add('ステイサムが撃退した！'), 6 * 1000;
-});
+setTimeout(() => log.addMessage('クジラを見た'), 3 * 1000);
+setTimeout(() => log.addMessage('船を見た'), 4 * 1000);
+setTimeout(() => log.addMessage('メガロドンが来た'), 5 * 1000);
+setTimeout(() => log.addMessage('ステイサムが撃退した！'), 6 * 1000);
 
+const logMessages = log.getMessages();
 setTimeout(() => {
     console.log(` --- 監視完了 (${new Date()}) ---`);
-    for (const entry of log) {
+    logMessages.forEach(entry => {
         const date = new Date(entry.timestamp);
         console.log(`${entry.message} (${date})`);
-    }
+    });
 }, 11 * 1000);
 
 /**
