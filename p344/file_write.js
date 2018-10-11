c35c7de369f3212bb946b116a6189ca5684a6921
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 16:54:26 +0900

    ifの判定後にブロックを追加

--- p344/file_write.js
+++ p344/file_write.js
@@ -4,5 +4,8 @@ const path = require('path');
 const message = 'ファイルへ書き込みをしましたよ\n';
 
 fs.writeFile(path.join(__dirname, 'hello.txt'), message, (err, data) => {
-    if (err) return console.lof('エラーファイルに書き込みできませんでした');
+    if (err) {
+        return console.lof('エラーファイルに書き込みできませんでした');
+    }
+    return data;
 });
