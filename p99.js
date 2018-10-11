4ca72ca24358c5594fc370f436fd526676056477
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 13:18:18 +0900

    関数を格納する変数を使ってチェック

--- p99.js
+++ p99.js
@@ -4,8 +4,11 @@ console.log(typeof {});
 console.log(typeof true);
 console.log(typeof 1);
 console.log(typeof '');
-console.log(typeof Symbol());
-console.log(typeof function() {});
+console.log(typeof Symbol('test symbol'));
+
+// typeofでfunctionを返すようにする
+const functionVal = () => 0;
+console.log(typeof functionVal);
 
 /**
  * typeof nullは[object]を返す
