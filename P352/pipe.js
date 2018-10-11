df970b09b0f2b442985524a4d34f05cb363c9b41
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 16:55:55 +0900

    ファイル名の指定文字列をテンプレートリテラルに変更 最終行を追加

--- P352/pipe.js
+++ P352/pipe.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-const readStream = fs.createReadStream(__dirname + "/stream.txt");
-const writeStream = fs.createWriteStream(__dirname + '/stream_copy.txt');
+const readStream = fs.createReadStream(`${__dirname}/stream.txt`);
+const writeStream = fs.createWriteStream(`${__dirname}/stream_copy.txt`);
 
 readStream.pipe(writeStream);
 
 /**
  * pipeのストリーム
  * データの移動によく活用されるとのこと
- *
+ */
