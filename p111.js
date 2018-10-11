3b168bf67fe031617ec4e4d60387cd99a82a75a7
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 13:44:57 +0900

    変数名変更

--- p111.js
+++ p111.js
@@ -2,13 +2,13 @@ function getSentence({ subject, verb, object }) {
     return `${subject} ${verb} ${object}`;
 }
 
-const o = {
+const sampleObject = {
     subject: 'I',
     verb: 'love',
     object: 'you',
 };
 
-console.log(getSentence(o));
+console.log(getSentence(sampleObject));
 /**
  * 分割代入を使えば、一気に変数への代入が可能
  * 配列でも同じことができる
