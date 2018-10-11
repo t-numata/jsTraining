50a86c680695e25d47f702c6b030face5723ef8e
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 10:44:38 +0900

    コロン・セミコロンの追加 --でのデクリメントを修正

--- p115-2.js
+++ p115-2.js
@@ -1,17 +1,17 @@
 const o = {
-    name:  "Alucard",
+    name: 'Alucard',
     greetbackwards() {
         const self = this;
         function getReverseName() {
-            let nameBackwards = "";
-            for (let i=self.name.length-1; i>=0; i--) {
+            let nameBackwards = '';
+            for (let i = self.name.length - 1; i >= 0; i -= 1) {
                 nameBackwards += self.name[i];
             }
             return nameBackwards;
         }
-        return `${getReverseName()} si eman ym ,olleH`
-    }
-}
+        return `${getReverseName()} si eman ym ,olleH`;
+    },
+};
 
 console.log(o.greetbackwards());
 
