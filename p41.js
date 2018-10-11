25fc92df3ef5e0d4225d49fc149d528fd8d8bef6
t-numata <2x.pypy.ktr@gmail.com>
2018-10-11 10:31:45 +0900

    シングルクォーテーション 最後の行を追加

--- p41.js
+++ p41.js
@@ -1,11 +1,11 @@
 const obj = {};
 
-obj.color = "黄色";
-obj["not an idenrifire"] = 3;
+obj.color = '黄色';
+obj['not an idenrifire'] = 3;
 
 const SIZE = Symbol();
 obj[SIZE] = 8;
 console.log(obj[SIZE]);
 obj.SIZE = 12;
-console.log(obj["SIZE"]);
+console.log(obj.SIZE);
 console.log(obj);
